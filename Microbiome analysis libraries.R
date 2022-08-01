924e6221e6d2ac515f883226a3db517e7e026b3a
abcervera <94945560+ABCervera@users.noreply.github.com>
2022-07-31 23:01:40 -0500

    Add files via upload

---
+++ Microbiome analysis libraries.R
@@ -0,0 +1,72 @@
+version
+getwd()
+BiocManager::install("devtools")
+BiocManager::install("dpylr")
+BiocManager::install("tidyverse")
+BiocManager::install("dpylr")
+install.packages("remotes") 
+devtools::install_github()
+
+install.packages(c("scales","ggridges","huxtable","gtsummary","magrittr"))
+install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+
+
+## try http if https is not available
+source("https://bioconductor.org/biocLite.R")
+biocLite("phyloseq")
+
+
+remotes::install_github("jbisanz/qiime2R")
+devtools::install_github("hadley/devtools")
+install.packages("remotes") 
+devtools::install_github("jbisanz/qiime2R")
+BiocManager::install("BiocInstaller")#si no lee qiime2R
+
+BiocManager::install(c("boot", "class",
+                       "codetools", "KernSmooth", 
+                       "lattice", "MASS", 
+                       "mgcv","nlme", 
+                       "nnet", "spatial", "survival"))
+
+devtools::install_github("phyloseq/joey711")
+BiocManager::install("phyloseq")
+install.packages (c("tidyr","dplyr","reshape2","splitstackshape"))
+devtools::install_github("ChiLiubio/microeco")
+BiocManager::install("microbiome")
+BiocManager::install("gplots")
+BiocManager::install("labdsv")
+install.packages(c("plotly","tidyverse","gplots"))
+
+
+library(ps)
+library(qiime2R)
+library(ranacapa)
+library(dplyr) 
+library(ggplot2)
+library(plyr)
+library(phyloseq)
+library(microeco)
+library(dplyr)
+library(reshape2)
+library(splitstackshape)
+
+library(tidyr)
+library(ggplot2)
+
+library(microbiome)
+
+
+library(labdsv)
+library(plotly)
+library(tidyverse)
+#library(DT)
+library(vegan)
+library( ggpubr )
+library(gplots)
+
+
+library(pairwiseAdonis)
+library(pubh)
+library(ggstance)
+library(ggformula)
+
